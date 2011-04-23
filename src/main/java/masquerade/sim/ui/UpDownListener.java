7ac3d1e597ab41c93e66455296cf3844db0e6ee9
Matt <matfsw@gmail.com>
2011-04-23 17:35:12 +0200

    Cleanup

--- src/main/java/masquerade/sim/ui/UpDownListener.java
+++ src/main/java/masquerade/sim/ui/UpDownListener.java
@@ -9,7 +9,9 @@ import com.vaadin.ui.Button.ClickListener;
 
 /**
  * A {@link ClickListener} for up/down buttons. Provides the ability
- * to move items in an {@link Indexed} container up and down.
+ * to move items in an {@link Indexed} container up and down. Set
+ * the select UI element provided to this listener to immediate
+ * to get button enablement to work correctly.
  */
 public class UpDownListener implements ClickListener {
 	private AbstractSelect viewer;
@@ -20,19 +22,38 @@ public class UpDownListener implements ClickListener {
 		this.isDown = isDown;
 	}
 
-	@Override public void buttonClick(ClickEvent event) {
+	/**
+	 * Handles clicks on up/down buttons, and moves the currently selected
+	 * item in the container accordingly.
+	 */
+	@Override 
+	public void buttonClick(ClickEvent event) {
 		Object selectionId = viewer.getValue();
+		if (selectionId == null) {
+			return;
+		}
+		
 		Container obj = (BeanItemContainer<?>) viewer.getContainerDataSource();
 		if (obj instanceof Indexed) {
 			Indexed container = (Indexed) obj;
-			int index = container.indexOfId(selectionId);
-			if (isDown && index < container.size() - 1) {
-				container.removeItem(selectionId);
-				container.addItemAt(index + 1, selectionId);
-			} else if (!isDown && index > 0) {
-				container.removeItem(selectionId);
-				container.addItemAt(index - 1, selectionId);
-			}
+			doMove(selectionId, container);
+		}
+	}
+
+	/**
+	 * Moves an item in the given indexed container up or down,
+	 * depending on {@link #isDown}.
+	 * @param selectionId
+	 * @param container
+	 */
+	private void doMove(Object selectionId, Indexed container) {
+		int index = container.indexOfId(selectionId);
+		if (isDown && index < container.size() - 1) {
+			container.removeItem(selectionId);
+			container.addItemAt(index + 1, selectionId);
+		} else if (!isDown && index > 0) {
+			container.removeItem(selectionId);
+			container.addItemAt(index - 1, selectionId);
 		}
 	}
 }
