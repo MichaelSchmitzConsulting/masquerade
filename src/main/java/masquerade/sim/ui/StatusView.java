a5462a621f262e3f3c6daa8d463c0cd6106863d5
Matt <matfsw@gmail.com>
2011-04-27 12:11:05 +0200

    Show stacktrace in status view on double click

--- src/main/java/masquerade/sim/ui/StatusView.java
+++ src/main/java/masquerade/sim/ui/StatusView.java
@@ -1,11 +1,18 @@
 package masquerade.sim.ui;
 
+import static org.apache.commons.lang.StringUtils.isEmpty;
+
+import java.awt.Dialog.ModalExclusionType;
 import java.util.Collection;
+
+import org.vaadin.codemirror.client.ui.CodeStyle;
 
 import masquerade.sim.status.Status;
 
 import com.vaadin.data.Container;
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.event.ItemClickEvent.ItemClickListener;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
@@ -53,6 +60,17 @@ public class StatusView extends VerticalLayout {
 		addComponent(table);
 		setExpandRatio(table, 1.0f);
 		
+		table.addListener(new ItemClickListener() {
+			@Override public void itemClick(ItemClickEvent event) {
+				if (event.isDoubleClick()) {
+					Status item = (Status) event.getItemId();
+					if  (item != null) {
+						showItemDetails(item);
+					}
+				}
+			}
+		});
+		
 		// Button layout
 		HorizontalLayout buttonLayout = new HorizontalLayout();
 		buttonLayout.setMargin(true, false, false, false);
@@ -69,5 +87,17 @@ public class StatusView extends VerticalLayout {
 		BeanItemContainer<Status> container = new BeanItemContainer<Status>(Status.class, statusLog);
 		container.sort(new String[]{ "time" }, new boolean[]{ false });
 		return container;
+	}
+
+	private void showItemDetails(Status item) {
+		String trace = item.getStacktrace();
+		String details;
+		if (isEmpty(trace)) {
+			details = item.getTimestamp() + " " + item.getMessage();
+		} else {
+			details = trace;
+		}
+		
+		SourceViewWindow.showModal(getWindow(), "Status Entry Details", details, CodeStyle.TEXT);
 	}
 }
