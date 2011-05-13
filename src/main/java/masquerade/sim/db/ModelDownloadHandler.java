3b35d93ad13388a70c0278adb4134e2951d62c9c
Matt <matfsw@gmail.com>
2011-05-13 16:49:01 +0200

    Named download file sensibly

--- src/main/java/masquerade/sim/db/ModelDownloadHandler.java
+++ src/main/java/masquerade/sim/db/ModelDownloadHandler.java
@@ -2,17 +2,24 @@ package masquerade.sim.db;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import masquerade.sim.ui.DownloadHandler;
+import masquerade.sim.util.NamedFileResource;
 
 import com.vaadin.Application;
-import com.vaadin.terminal.FileResource;
 import com.vaadin.terminal.Resource;
 
+/**
+ * Exports the simulation configuration to a temporary
+ * file and provides it as a download (using a {@link Resource})
+ * to the client.
+ */
 public class ModelDownloadHandler implements DownloadHandler {
 
-	private ModelExport exporter;
-	private Application application;
+	private final ModelExport exporter;
+	private final Application application;
 
 	public ModelDownloadHandler(ModelExport exporter, Application application) {
 		this.exporter = exporter;
@@ -23,8 +30,18 @@ public class ModelDownloadHandler implements DownloadHandler {
 	public Resource provideDownload() throws IOException {
 		File tempFile = File.createTempFile("masquerade-", "-download");
 		exporter.exportModel(tempFile);
+		
 		// TODO: Limit/cleanup temp files
-		return new FileResource(tempFile, application);
+		tempFile.deleteOnExit();
+		
+		final String fileName = "masquerade-export." + timestamp() + ".simulation";
+		final String mimeType = "application/x-masquerade-simulation";
+		
+		return new NamedFileResource(tempFile, application, mimeType, fileName);
+	}
+
+	private String timestamp() {
+		return new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss.SSS").format(new Date());
 	}
 
 }
