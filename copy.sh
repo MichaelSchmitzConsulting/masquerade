7d9e4587247771b9b09c5eaaa24f0bd059d5d9ba
Matt <matfsw@gmail.com>
2011-06-30 11:10:11 +0200

    Split masquerade into separate module bundles, change this project to be a maven parent pom module only.

---
+++ copy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp -v ../masquerade.sim.model/target/*.jar ../masquerade/src/main/webapp/WEB-INF/bundles
+cp -v ../masquerade.sim.core/target/*.jar ../masquerade/src/main/webapp/WEB-INF/bundles
+cp -v ../masquerade.sim.application/target/*.jar ../masquerade/src/main/webapp/WEB-INF/bundles
+cp -v ../masquerade.sim.channel/target/*.jar ../masquerade/src/main/webapp/WEB-INF/bundles
+cp -v ../masquerade.sim.channel.jms/target/*.jar ../masquerade/src/main/webapp/WEB-INF/bundles
