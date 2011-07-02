9bb17907f1ee3d46a1f9f355417f2ea198b128cb
Matt <matfsw@gmail.com>
2011-07-02 15:10:30 +0200

    Updated for new module names, added mkdir

--- copy.sh
+++ copy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-cp -v ../masquerade.sim.model/target/*.jar ../masquerade/src/main/webapp/WEB-INF/bundles
-cp -v ../masquerade.sim.core/target/*.jar ../masquerade/src/main/webapp/WEB-INF/bundles
-cp -v ../masquerade.sim.application/target/*.jar ../masquerade/src/main/webapp/WEB-INF/bundles
-cp -v ../masquerade.sim.channel/target/*.jar ../masquerade/src/main/webapp/WEB-INF/bundles
-cp -v ../masquerade.sim.channel.jms/target/*.jar ../masquerade/src/main/webapp/WEB-INF/bundles
+mkdir -p ../masquerade.sim.webapp/src/main/webapp/WEB-INF/bundles/
+cp -v ../masquerade.sim.model/target/*.jar ../masquerade.sim.webapp/src/main/webapp/WEB-INF/bundles/
+cp -v ../masquerade.sim.core/target/*.jar ../masquerade.sim.webapp/src/main/webapp/WEB-INF/bundles/
+cp -v ../masquerade.sim.application/target/*.jar ../masquerade.sim.webapp/src/main/webapp/WEB-INF/bundles/
+cp -v ../masquerade.sim.channel/target/*.jar ../masquerade.sim.webapp/src/main/webapp/WEB-INF/bundles/
+cp -v ../masquerade.sim.channel.jms/target/*.jar ../masquerade.sim.webapp/src/main/webapp/WEB-INF/bundles/
