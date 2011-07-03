affe5a3ba3cbad5471f90a088999de4b00358481
Matt <matfsw@gmail.com>
2011-07-03 09:21:25 +0200

    Added ActiveMQ bundle

--- copy.sh
+++ copy.sh
@@ -5,3 +5,4 @@ cp -v ../masquerade.sim.core/target/*.jar ../masquerade.sim.webapp/src/main/weba
 cp -v ../masquerade.sim.application/target/*.jar ../masquerade.sim.webapp/src/main/webapp/WEB-INF/bundles/
 cp -v ../masquerade.sim.channel/target/*.jar ../masquerade.sim.webapp/src/main/webapp/WEB-INF/bundles/
 cp -v ../masquerade.sim.channel.jms/target/*.jar ../masquerade.sim.webapp/src/main/webapp/WEB-INF/bundles/
+cp -v ../masquerade.jms.activemq/target/*.jar ../masquerade.sim.webapp/src/main/webapp/WEB-INF/bundles/
