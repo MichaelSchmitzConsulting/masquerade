a3bfc9afaad2b77309d0f7727d303e1227377b0b
Matt <matfsw@gmail.com>
2011-07-20 17:16:24 +0200

    Pass arguments to mvn (mainly for -o)

--- all.sh
+++ all.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-rm -rf ../masquerade.sim.webapp/src/main/webapp/WEB-INF/bundles/
-mvn eclipse:eclipse clean install && ./copy.sh
+rm -r ../masquerade.sim.webapp/src/main/webapp/WEB-INF/bundles/
+mvn $@ eclipse:eclipse clean install && ./copy.sh
