b556f32bee05f3a7022e797aa57b62f10927b62f
Matt <matfsw@gmail.com>
2011-09-15 20:59:00 +0200

    Use first param as since

--- log.sh
+++ log.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find .. -d 1 -name "masquerade*" | xargs -t -L 1 -I {} git --git-dir={}/.git --no-pager log --oneline $@
+find .. -d 1 -name "masquerade*" | xargs -t -L 1 -I {} git --git-dir={}/.git --no-pager log --oneline $1..
